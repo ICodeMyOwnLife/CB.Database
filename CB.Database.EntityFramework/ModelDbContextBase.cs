907e494f5c040fdd72eaab31ce7fa58783f0f774
ICodeMyOwnLife <icodemyownlife@gmail.com>
2016-04-10 09:04:39 +0700

    Add CB.Database.EntityFramework

---
+++ CB.Database.EntityFramework/ModelDbContextBase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data.Entity;
+using System.Threading.Tasks;
+
+
+namespace CB.Database.EntityFramework
+{
+    public abstract class ModelDbContextBase<TDbContext> where TDbContext: DbContext, new()
+    {
+        protected virtual Task<TResult> FetchDataContextAsync<TResult>(Func<TDbContext, Task<TResult>> fetchContextAsync)
+        {
+            return FetchDataContext(fetchContextAsync);
+        }
+
+        protected virtual TResult FetchDataContext<TResult>(Func<TDbContext, TResult> fetchContext)
+        {
+            using (var context = new TDbContext())
+            {
+                return fetchContext(context);
+            }
+        }
+
+        protected virtual void UseDataContext(Action<TDbContext> useContext)
+        {
+            FetchDataContext<object>(context =>
+            {
+                useContext(context);
+                return null;
+            });
+        }
+
+/*        private static Task UseDataContextAsync(Func<TDbContext, Task> useContextAsync)
+        {
+            return FetchDataContextAsync<Task>(context => useContextAsync(context));
+        }*/
+    }
+}
