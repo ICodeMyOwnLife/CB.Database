50f7fed9d2e7a0195e64db7770f978af858250ff
ICodeMyOwnLife <icodemyownlife@gmail.com>
2016-04-30 16:16:20 +0700

    Change SaveModel

--- CB.Database.EntityFramework/ModelDbContextBase.cs
+++ CB.Database.EntityFramework/ModelDbContextBase.cs
@@ -4,14 +4,87 @@ using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 using System.Threading.Tasks;
 using CB.Model.Common;
 
 
 namespace CB.Database.EntityFramework
 {
+    public class ModelInfo
+    {
+        #region  Properties & Indexers
+        public PropertyInfo Id { get; set; }
+        public PropertyInfo Property { get; set; }
+        public PropertyInfo PropertyId { get; set; }
+        #endregion
+
+
+        #region Methods
+        public object GetProperty(object source)
+            => Property.GetValue(source);
+        #endregion
+    }
+
+    public class DependentPropertyDictionary
+    {
+        #region Fields
+        readonly IDictionary<Type, IList<ModelInfo>> _dependentDictionary =
+            new Dictionary<Type, IList<ModelInfo>>();
+        #endregion
+
+
+        #region Methods
+        public void Add<TModel>(PropertyInfo property, PropertyInfo id, PropertyInfo propertyId)
+            => Add(typeof(TModel), property, id, propertyId);
+
+        public void Add<TModel, TProperty, TId>(Expression<Func<TModel, TProperty>> propertyExpression,
+            Expression<Func<TModel, TId>> idExpression, Expression<Func<TProperty, TId>> propertyIdExpression)
+            =>
+                Add<TModel>(propertyExpression.GetPropertyInfo(), idExpression.GetPropertyInfo(),
+                    propertyIdExpression.GetPropertyInfo());
+
+        public IEnumerable<object> GetProperties<TModel>(TModel model)
+        {
+            if (model == null) return null;
+            var modelType = model.GetType();
+            return _dependentDictionary.ContainsKey(modelType)
+                       ? _dependentDictionary[modelType].Select(mi => mi.GetProperty(model)) : null;
+        }
+        #endregion
+
+
+        #region Implementation
+        private void Add(Type type, PropertyInfo property, PropertyInfo id, PropertyInfo propertyId)
+        {
+            IList<ModelInfo> dependentProperties;
+
+            if (!_dependentDictionary.ContainsKey(type))
+            {
+                dependentProperties = new List<ModelInfo>();
+                _dependentDictionary[type] = dependentProperties;
+            }
+            else
+            {
+                dependentProperties = _dependentDictionary[type];
+            }
+            dependentProperties.Add(new ModelInfo
+            {
+                Property = property,
+                Id = id,
+                PropertyId = propertyId
+            });
+        }
+        #endregion
+    }
+
     public abstract class ModelDbContextBase<TDbContext> where TDbContext: DbContext, new()
     {
+        #region Fields
+        private readonly DependentPropertyDictionary _dependentPropertyDictionary = new DependentPropertyDictionary();
+        #endregion
+
+
         #region Implementation
         protected virtual TDbContext CreateContext()
         {
@@ -255,10 +328,24 @@ namespace CB.Database.EntityFramework
         private static void MarkDeletedModel<TModel>(TModel model, TDbContext context) where TModel: class
             => context.Entry(model).State = EntityState.Deleted;
 
-        private static void MarkSavedModel<TModel>(TModel model, TDbContext context) where TModel: IdModelBase
-            => context.Entry(model).State = !model.Id.HasValue || model.Id.Value == 0
-                                                ? EntityState.Added
-                                                : EntityState.Modified;
+        private void MarkSavedModel<TModel>(TModel model, TDbContext context) where TModel: IdModelBase
+        {
+            context.Entry(model).State = !model.Id.HasValue || model.Id.Value == 0
+                                             ? EntityState.Added
+                                             : EntityState.Modified;
+            var modelProperties = _dependentPropertyDictionary.GetProperties(model);
+            if (modelProperties == null) return;
+
+            foreach (var property in modelProperties.Where(p => p != null))
+            {
+                context.Entry(property).State = EntityState.Unchanged;
+            }
+        }
+
+        protected void RegisterDependent<TModel, TProperty, TId>(
+            Expression<Func<TModel, TProperty>> propertyExpression, Expression<Func<TModel, TId>> idExpression,
+            Expression<Func<TProperty, TId>> propertyIdExpression)
+            => _dependentPropertyDictionary.Add(propertyExpression, idExpression, propertyIdExpression);
 
         protected virtual TModel SaveModel<TModel>(TModel model) where TModel: IdModelBase
             => FetchDataContext(context =>
---
+++ CB.Database.TestEntityFramwork/Student.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using CB.Model.Common;
+
+
+namespace CB.Database.TestEntityFramwork
+{
+    [Serializable]
+    public class Student: IdNameModelBase
+    {
+        #region Fields
+        [NonSerialized]
+        private ICollection<Class> _classes;
+        #endregion
+
+
+        #region  Properties & Indexers
+        [XmlIgnore]
+        public ICollection<Class> Classes
+        {
+            get { return _classes; }
+            set { SetProperty(ref _classes, value); }
+        }
+        #endregion
+    }
+
+    public class Class: IdNameModelBase
+    {
+        #region Fields
+        [NonSerialized]
+        private ICollection<Student> _students;
+        #endregion
+
+
+        #region  Properties & Indexers
+        [XmlIgnore]
+        public ICollection<Student> Students
+        {
+            get { return _students; }
+            set { SetProperty(ref _students, value); }
+        }
+        #endregion
+    }
+}
+
+
+// TODO: Test New SaveModel (One-to-many relationship and many-to-many relationship)
